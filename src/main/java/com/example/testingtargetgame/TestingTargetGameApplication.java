36d8a66bdcd71f6e3c8431f186a9f1761d660128
Samuel Lee <samuelxlee@gmail.com>
2021-10-29 10:34:29 -0700

    created project

---
+++ src/main/java/com/example/testingtargetgame/TestingTargetGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingtargetgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTargetGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingTargetGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testingtargetgame/TestingTargetGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingtargetgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingTargetGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
